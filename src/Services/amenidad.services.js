4372aca0a4b7a81773fee99ca7741f8a5928458d
Jo Huerta <santiago@alimx.mx>
2022-10-06 23:50:49 -0500

    se eliminan componentes que no me sirven

--- src/Services/amenidad.services.js
+++ src/Services/amenidad.services.js
@@ -1,17 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 
 const baseURL = process.env.BASEAMENIDAD;
 
 const MY_SERVICE = axios.create({
-    baseURL,
-    timeout: 1000,
-  });
+  baseURL,
+  timeout: 1000,
+});
 
-  const AMENIDAD_SERVICE = {
-    addAmenidades:async(amenidades)=>{
-        const res = await MY_SERVICE.post('/addAmenidades',amenidades)
-        return res;
-    }
-  }
+const AMENIDAD_SERVICE = {
+  addAmenidades: async (amenidades) => {
+    const res = await MY_SERVICE.post('/addAmenidades', amenidades);
+    return res;
+  },
+};
 
-  export default AMENIDAD_SERVICE;
+export default AMENIDAD_SERVICE;
--- src/views/Residente/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const index = () => <div>Residente</div>;
-
-export default index;
